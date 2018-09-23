200a263c0c363502ab92dd2b4d328573a7223e05
Paul McNeely <paul@paulmcneely.com>
2018-09-23 00:51:37 -0500

    Why aren't you dead yet? (Travis)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+require __DIR__.'/../vendor/autoload.php';
 
 if (!class_exists('PHPUnit\Framework\Error\Warning')) {
     class_alias('PHPUnit_Framework_Error_Warning', 'PHPUnit\Framework\Error\Warning');
